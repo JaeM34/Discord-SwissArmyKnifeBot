73d8f195db97362a76d60511be2924396cc9a014
Justina Molina <jmolina0095@gmail.com>
2024-07-29 15:09:55 -0700

    Created bot object

--- bot.py
+++ bot.py
@@ -3,3 +3,14 @@
 #   INITIALIZE ANY INTERNALIZED CLOCKS
 #   BEGIN LOGIC LOOP
 #   CREATE LOG
+import pymongo
+import dotenv
+import discord
+
+token = dotenv.get_key("discord_token")
+client = discord.client(token)
+
+class bot: 
+    def run():
+        print("hello")
+        
